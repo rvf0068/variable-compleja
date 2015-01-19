e73df1a280c43d59da60458fb610f59d41f53381
Rafael Villarroel <rvf0068@gmail.com>
2015-01-19 08:46:51 -0600

    Site updated at 2015-01-19 14:46:51 UTC

---
+++ org/auto/2015-01-19-los-numeros-complejos.el
@@ -0,0 +1,41 @@
+(TeX-add-style-hook
+ "2015-01-19-los-numeros-complejos"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "spanish" "presentation")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("libertine" "mono=false") ("luximono" "scaled=0.7") ("babel" "spanish" "mexico" "es-noshorthands" "english")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "capt-of"
+    "listings"
+    "libertine"
+    "luximono"
+    "lxfonts"
+    "babel"
+    "enumitem"
+    "tikz"
+    "tikz-cd"
+    "pgfplots")
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-2"
+    "sec-3"
+    "sec-4")))
+
