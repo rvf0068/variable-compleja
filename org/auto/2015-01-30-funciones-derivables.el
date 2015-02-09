8b9eb5d99ce4fbb32bd4302f2c8456ac60e4b374
Rafael Villarroel <rvf0068@gmail.com>
2015-02-09 07:55:29 -0600

    Site updated at 2015-02-09 13:55:29 UTC

---
+++ org/auto/2015-01-30-funciones-derivables.el
@@ -0,0 +1,52 @@
+(TeX-add-style-hook
+ "2015-01-30-funciones-derivables"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "spanish" "presentation")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("libertine" "mono=false") ("luximono" "scaled=0.7") ("babel" "spanish" "mexico" "es-noshorthands" "english")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "inputenc"
+    "fontenc"
+    "fixltx2e"
+    "graphicx"
+    "longtable"
+    "float"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "marvosym"
+    "wasysym"
+    "amssymb"
+    "capt-of"
+    "listings"
+    "libertine"
+    "luximono"
+    "lxfonts"
+    "babel"
+    "enumitem"
+    "tikz"
+    "tikz-cd"
+    "pgfplots")
+   (LaTeX-add-labels
+    "sec-1"
+    "sec-2"
+    "sec-3")))
+
